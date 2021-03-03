e716e1451de14b48f589e1f32a2a3d55bca548f2
nghia <=>
2021-03-04 00:06:18 +0700

    refactor

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var database = require('./database')
 
 var app = express();
 
+app.get('/', function(req, res) {
+    res.send('Xin chao');
+});
+
 app.get('/listtuyenxe', function(req, res) {
     database.getAll(function(results) {
         res.json(results)
