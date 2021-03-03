e4c60f32558c43722cf3600c9e12f8caa4ec9f4f
nghia <=>
2021-03-04 00:01:23 +0700

    Sua doi config port

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.get('/listtuyenxe', function(req, res) {
     database.getAll(function(results) {
         res.json(results)
     })
-})
-console.log('sua doi');
-app.listen(3000, function() {
-    console.log('Node server running @ http://localhost:8888')
-})
+});
+
+app.listen(process.env.PORT, function() {
+    console.log('Node server running @ http://localhost:'+process.env.PORT);
+});
